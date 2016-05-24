266869fee2596d9a246588203f98b324b04969c9
rodrigo <rodrigoderosa@outlook.com>
2016-05-24 18:15:02 -0300

    Vida

---
+++ AlgoFormers/src/algoFormers/modelo/personajes/Vida.java
@@ -0,0 +1,15 @@
+package algoFormers.modelo.personajes;
+
+public class Vida {
+
+	private int vida;
+	
+	public Vida(int vida){
+		this.setVida(vida);
+	}
+	
+	private void setVida(int vida){
+		this.vida = vida;
+	}
+	
+}
