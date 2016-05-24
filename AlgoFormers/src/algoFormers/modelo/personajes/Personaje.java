300cefaae10e76c65323ff029425610ce9b71be1
rodrigo <rodrigoderosa@outlook.com>
2016-05-24 17:56:05 -0300

    public static int HUMANOIDE

--- AlgoFormers/src/algoFormers/modelo/personajes/Personaje.java
+++ AlgoFormers/src/algoFormers/modelo/personajes/Personaje.java
@@ -4,6 +4,7 @@ public abstract class Personaje {
 
 	protected Transformable estado;
 	public static int ALTERNO = 0;
+	public static int HUMANOIDE = 1;
 	
 	public Personaje(){
 		this.setEstado(ALTERNO);
