06885bc4f9407780f5791d26e62ceb3e2148c863
rodrigo <rodrigoderosa@outlook.com>
2016-05-24 17:56:05 -0300

    vida, ataque, distancia

--- AlgoFormers/src/algoFormers/modelo/personajes/Personaje.java
+++ AlgoFormers/src/algoFormers/modelo/personajes/Personaje.java
@@ -4,6 +4,11 @@ public abstract class Personaje {
 
 	protected Transformable estado;
 	public static int ALTERNO = 0;
+	public static int HUMANOIDE = 1;
+	protected int vida;
+	protected int ataque;
+	protected int distanciaDeAtaque;
+	protected int velocidad;
 	
 	public Personaje(){
 		this.setEstado(ALTERNO);
