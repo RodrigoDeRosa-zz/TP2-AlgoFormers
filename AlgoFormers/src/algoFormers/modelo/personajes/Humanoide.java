7a72ffdf73911eb1464a0128f103e50e90ec8709
marc <marc@localhost>
2016-05-25 12:59:19 -0300

    Agregue la clase estado abstracta y sus dos hijos Alterno y Humanoide. Y solo cambie la de Personaje. las de Bumblebee no las toque. El constructor de personaje no necesariamente tiene que ser asi (mas si vamos a hace singleton con los personajes) pero es para que se entienda la idea

---
+++ AlgoFormers/src/algoFormers/modelo/personajes/Humanoide.java
@@ -0,0 +1,10 @@
+package algoFormers.modelo.personajes;
+
+public class Humanoide extends Estado {
+
+	public Humanoide(int vida, int ataque, int distanciaAtaque, int velocidad){
+		
+		super(vida, ataque, distanciaAtaque, velocidad);
+		
+	}
+}
--- AlgoFormers/src/algoFormers/modelo/personajes/Personaje.java
+++ AlgoFormers/src/algoFormers/modelo/personajes/Personaje.java
@@ -2,13 +2,24 @@ package algoFormers.modelo.personajes;
 
 public abstract class Personaje {
 
-	protected Estado estado;
+	protected Estado estadoActual;
+	protected Estado estadoOtro;
 	
-	public Personaje(){
-		this.setEstado();
+	public Personaje(int vidaH, int ataqueH, int distanciaAtaqueH, int velocidadH, int vidaA, int ataqueA, int distanciaAtaqueA, int velocidadA){
+	
+		//supuesto: el estado actual por defecto es el humanoide
+		estadoActual = new Humanoide(vidaH, ataqueH, distanciaAtaqueH, velocidadH);
+		estadoOtro = new Alterno(vidaA, ataqueA, distanciaAtaqueA, velocidadA);
+		
 	}
 	
 	abstract protected void setEstado();
 	abstract public Estado getEstado();
-	abstract public void cambiarEstado();
+	public void cambiarEstado(){
+		
+		Estado estadoAnterior = estadoActual;
+		estadoActual = estadoOtro;
+		estadoOtro = estadoAnterior;
+		
+	};
 }
