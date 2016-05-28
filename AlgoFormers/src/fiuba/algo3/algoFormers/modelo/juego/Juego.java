414cf28e6251cbc79845311c133cf41387eb8749
Prueba <Prueba@Mateo-PC>
2016-05-28 13:58:26 -0300

    Agregada la interfaz ubicable

--- AlgoFormers/src/fiuba/algo3/algoFormers/modelo/juego/Juego.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/juego/Juego.java
@@ -4,12 +4,12 @@ import fiuba.algo3.algoFormers.modelo.mapas.Posicion;
 
 public class Juego {
 
-	public Object getUbicable(Posicion posicion) {
+	public Ubicable getUbicable(Posicion posicion) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	public Posicion getPosicionChispa() {
+	public Ubicable getPosicionChispa() {
 		// TODO Auto-generated method stub
 		return null;
 	}
---
+++ AlgoFormers/test/fiuba/algo3/algoFormers/entrega1/JuegoTest.java
@@ -0,0 +1,19 @@
+package fiuba.algo3.algoFormers.entrega1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import fiuba.algo3.algoFormers.modelo.juego.Juego;
+
+public class JuegoTest {
+
+	@Test
+	public void crearJuegoTest() {
+
+		Juego juego = new Juego();
+		assertNotNull(juego);
+		
+	}
+
+}
