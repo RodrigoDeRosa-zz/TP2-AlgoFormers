6a6bf6d56c33f42d728944312c2e3b5ce648a818
facundo <facundo@facundo-System-Product-Name>
2016-05-28 19:43:14 -0300

    Cambio de ubicable

--- AlgoFormers/src/fiuba/algo3/algoFormers/modelo/personajes/AutoBot.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/personajes/AutoBot.java
@@ -1,6 +1,7 @@
 package fiuba.algo3.algoFormers.modelo.personajes;
 
 import fiuba.algo3.algoFormers.modelo.excepciones.FuegoAmigoException;
+import fiuba.algo3.algoFormers.modelo.interfaces.Ubicable;
 import fiuba.algo3.algoFormers.modelo.personajes.estadosDeTransformacion.Alterno;
 
 public class AutoBot extends AlgoFormer {
@@ -9,12 +10,12 @@ public class AutoBot extends AlgoFormer {
 		super(vida, alterno, velocidadH, ataqueH, distAtaqueH);
 	}
 	
-	public void recibirDanio(AutoBot autobot, int poderAtaque){
-		throw new FuegoAmigoException();
+	public void recibirDanio(Ubicable ubicable, int poderAtaque){
+		
+		if(ubicable.getClass() == this.getClass()){
+			throw new FuegoAmigoException();
+		}
+		this.vida.descontarVida(poderAtaque);
 	}
 	
-	public void recibirDanio(Decepticon decepticon, int poderAtaque){
-		this.vida.descontarVida(poderAtaque);
-		
-	}
 }
