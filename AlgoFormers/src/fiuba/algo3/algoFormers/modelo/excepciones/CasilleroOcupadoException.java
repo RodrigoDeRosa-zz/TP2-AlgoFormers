3c9b09fba10fdabc3c850e91f6084e6a57851281
marc <marc@localhost>
2016-05-28 14:45:28 -0300

    La clase casillero y mapa mas completas

---
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/excepciones/CasilleroOcupadoException.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.algoFormers.modelo.excepciones;
+
+public class CasilleroOcupadoException extends RuntimeException {
+
+	public CasilleroOcupadoException(String mensaje){
+		super(mensaje);
+	}
+	
+}
--- AlgoFormers/src/fiuba/algo3/algoFormers/modelo/mapas/Casillero.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/mapas/Casillero.java
@@ -1,8 +1,35 @@
 package fiuba.algo3.algoFormers.modelo.mapas;
 
+import fiuba.algo3.algoFormers.modelo.interfaces.Ubicable;
+
 public class Casillero {
 
+	private Ubicable ubicable;
+	private boolean ocupado;
+	
 	public Casillero(){
+		
+		ocupado = false;
+		
+	}
+	
+	public void setUbicable(Ubicable unUbicable){
+		
+		this.ubicable = unUbicable;
+		ocupado = true;
+		
+		
+	}
+
+	public boolean estaOcupado() {
+		
+		return ocupado;
+	
+	}
+
+	public Ubicable getUbicable() {
+		
+		return ubicable;
 		
 	}
 }
