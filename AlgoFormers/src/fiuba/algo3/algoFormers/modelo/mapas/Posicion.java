c92a2e2ca06bab90045276e0667df691c45574e7
marc <marc@localhost>
2016-05-28 14:13:28 -0300

    agregue 2 pruebas funcionando del mapa

--- AlgoFormers/src/fiuba/algo3/algoFormers/modelo/mapas/Posicion.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/mapas/Posicion.java
@@ -2,22 +2,54 @@ package fiuba.algo3.algoFormers.modelo.mapas;
 
 public class Posicion {
 
+	private int x;
+	private int y;
+
+
 	public Posicion(int i, int j) {
-		// TODO Auto-generated constructor stub
+		x = i;
+		y = j;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + x;
+		result = prime * result + y;
+		return result;
+	}
+
+
+	//Metodos de comparacion.
 	
-	//Metodos Para Pruebas
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Posicion other = (Posicion) obj;
+		if (x != other.x)
+			return false;
+		if (y != other.y)
+			return false;
+		return true;
+	}
 	
+	
+	
+	//Metodos para pruebas.
+
 	public int getX() {
-		// TODO Auto-generated method stub
-		return 0;
+		return x;
 	}
 
 
 	public int getY() {
-		// TODO Auto-generated method stub
-		return 0;
+		return y;
 	}
 
 }
