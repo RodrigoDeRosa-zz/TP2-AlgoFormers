f6f51ac9e7929dd953e80f12da455cffa6331cb7
Prueba <Prueba@Mateo-PC>
2016-05-30 04:47:16 -0300

    Agregado cantidad de movimientos disponibles al manejador de movimientos

--- AlgoFormers/src/fiuba/algo3/algoFormers/modelo/personajes/manejadores/ManejadorDeMovimientos.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/personajes/manejadores/ManejadorDeMovimientos.java
@@ -2,6 +2,7 @@ package fiuba.algo3.algoFormers.modelo.personajes.manejadores;
 
 import fiuba.algo3.algoFormers.modelo.direcciones.Direccion;
 import fiuba.algo3.algoFormers.modelo.excepciones.CasilleroOcupadoException;
+import fiuba.algo3.algoFormers.modelo.excepciones.NoQuedanMovimientosDisponiblesException;
 import fiuba.algo3.algoFormers.modelo.excepciones.PosicionFueraDelMapaException;
 import fiuba.algo3.algoFormers.modelo.mapas.Mapa;
 import fiuba.algo3.algoFormers.modelo.mapas.Posicion;
@@ -12,16 +13,23 @@ public class ManejadorDeMovimientos {
 	private Posicion posicionOrigen, posicionDestino;
 	private Algoformer algoformer;
 	private Mapa mapa;
+	private int movimientosDisponibles;
 	
 	public ManejadorDeMovimientos(Mapa mapaDelJuego, Algoformer algoformer, Direccion dir) {
 		
 		posicionOrigen = algoformer.getPosicion();
 		posicionDestino = posicionOrigen.sumarDireccion(dir);
 		mapa = mapaDelJuego;
+		movimientosDisponibles = algoformer.getPuntosVelocidad();
 		
 	}
 	
 	public void RealizarMovimiento() {
+		
+		if (movimientosDisponibles <= 0) {
+			
+			throw new NoQuedanMovimientosDisponiblesException();
+		}
 		
 		try {
 			mapa.ubicar(algoformer, posicionDestino);
@@ -32,7 +40,8 @@ public class ManejadorDeMovimientos {
 		catch (CasilleroOcupadoException e) {
 			throw e;
 		}
-		mapa.desocupar(posicionOrigen);	
+		mapa.desocupar(posicionOrigen);
+		movimientosDisponibles--;
 		
 	}
 	
