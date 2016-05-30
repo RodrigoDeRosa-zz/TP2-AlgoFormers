8a5d1dc6598ed30b46e0ac6ac2034f0d441dfe8d
rodrigo <rodrigoderosa@outlook.com>
2016-05-30 15:37:46 -0300

    agregada Chispa

---
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/capturables/Chispa.java
@@ -0,0 +1,9 @@
+package fiuba.algo3.algoFormers.modelo.capturables;
+
+public class Chispa {
+
+	public Chispa(){
+		
+	}
+	
+}
--- AlgoFormers/src/fiuba/algo3/algoFormers/modelo/interfaces/Ubicable.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/interfaces/Ubicable.java
@@ -1,6 +1,5 @@
 package fiuba.algo3.algoFormers.modelo.interfaces;
 
-import fiuba.algo3.algoFormers.modelo.personajes.AlgoFormer;
-
 public interface Ubicable {
+	
 }
