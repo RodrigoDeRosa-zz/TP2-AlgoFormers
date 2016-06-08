916f5d38bf7501ca34d567610236f74cdc743190
marc <marc@localhost>
2016-06-08 16:13:41 -0300

    Mas test de la entrega 2 hechos

--- AlgoFormers/test/fiuba/algo3/algoFormers/entrega2/EntregaDosTest.java
+++ AlgoFormers/test/fiuba/algo3/algoFormers/entrega2/EntregaDosTest.java
@@ -60,6 +60,81 @@ public class EntregaDosTest {
 	}
 	
 	@Test
+	public void testRatchetHumanoideAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(ratchet, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		ratchet.moverEnDireccion(direccion, mapa);	
+		assertTrue(ratchet.getVida() == 150);
+		assertTrue(ratchet.getAtaque() == 5);
+		assertTrue(ratchet.getDistanciaDeAtaque() == 5);
+		assertTrue(ratchet.getVelocidad() == 1);		
+	}
+	
+	@Test
+	public void testBumblebeeHumanoideAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(bumblebee, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		bumblebee.moverEnDireccion(direccion, mapa);	
+		assertTrue(bumblebee.getVida() == 350);
+		assertTrue(bumblebee.getAtaque() == 40);
+		assertTrue(bumblebee.getDistanciaDeAtaque() == 1);
+		assertTrue(bumblebee.getVelocidad() == 2);		
+	}
+	
+	@Test
+	public void testMegatronHumanoideAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(megatron, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		megatron.moverEnDireccion(direccion, mapa);	
+		assertTrue(megatron.getVida() == 550);
+		assertTrue(megatron.getAtaque() == 10);
+		assertTrue(megatron.getDistanciaDeAtaque() == 3);
+		assertTrue(megatron.getVelocidad() == 1);		
+	}
+	
+	@Test
+	public void testFrenzyHumanoideAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(frenzy, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		frenzy.moverEnDireccion(direccion, mapa);	
+		assertTrue(frenzy.getVida() == 400);
+		assertTrue(frenzy.getAtaque() == 10);
+		assertTrue(frenzy.getDistanciaDeAtaque() == 5);
+		assertTrue(frenzy.getVelocidad() == 2);		
+	}
+	
+	@Test
+	public void testBonecrusherHumanoideAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(bonecrusher, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		bonecrusher.moverEnDireccion(direccion, mapa);	
+		assertTrue(bonecrusher.getVida() == 200);
+		assertTrue(bonecrusher.getAtaque() == 30);
+		assertTrue(bonecrusher.getDistanciaDeAtaque() == 3);
+		assertTrue(bonecrusher.getVelocidad() == 1);		
+	}
+	
+	@Test
 	public void testOptimusAlternoAtraviezaZonaRocosa(){
 		SuperficieRocosa superficie = factoryS.getRocosa();
 		Posicion posicion1 = new Posicion(1,1);
@@ -74,8 +149,86 @@ public class EntregaDosTest {
 		assertTrue(optimus.getDistanciaDeAtaque() == 4);
 		assertTrue(optimus.getVelocidad() == 5);
 	}
-
-		//FALTAN LOS OTROS 5 ALGOFORMERS
+	
+	@Test
+	public void testRatchetAlternoAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(ratchet, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		ratchet.transformarse();
+		ratchet.moverEnDireccion(direccion, mapa);	
+		assertTrue(ratchet.getVida() == 150);
+		assertTrue(ratchet.getAtaque() == 35);
+		assertTrue(ratchet.getDistanciaDeAtaque() == 2);
+		assertTrue(ratchet.getVelocidad() == 8);		
+	}
+	
+	@Test
+	public void testBumblebeeAlternoAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(bumblebee, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		bumblebee.transformarse();
+		bumblebee.moverEnDireccion(direccion, mapa);
+		assertTrue(bumblebee.getVida() == 350);
+		assertTrue(bumblebee.getAtaque() == 20);
+		assertTrue(bumblebee.getDistanciaDeAtaque() == 3);
+		assertTrue(bumblebee.getVelocidad() == 5);		
+	}
+	
+	@Test
+	public void testMegatronAlternoAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(megatron, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		megatron.transformarse();
+		megatron.moverEnDireccion(direccion, mapa);	
+		assertTrue(megatron.getVida() == 550);
+		assertTrue(megatron.getAtaque() == 55);
+		assertTrue(megatron.getDistanciaDeAtaque() == 2);
+		assertTrue(megatron.getVelocidad() == 8);		
+	}
+	
+	@Test
+	public void testFrenzyAlternoAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(frenzy, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		frenzy.transformarse();
+		frenzy.moverEnDireccion(direccion, mapa);	
+		assertTrue(frenzy.getVida() == 400);
+		assertTrue(frenzy.getAtaque() == 25);
+		assertTrue(frenzy.getDistanciaDeAtaque() == 2);
+		assertTrue(frenzy.getVelocidad() == 6);		
+	}
+	
+	@Test
+	public void testBonecrusherAlternoAtraviezaZonaRocosa(){
+		SuperficieRocosa superficie = factoryS.getRocosa();
+		Posicion posicion1 = new Posicion(1,1);
+		Posicion posicion2 = new Posicion(1,2);
+		mapa.ubicarAtacable(bonecrusher, posicion1);
+		mapa.ubicarSuperficie(superficie, posicion2);
+		DirArriba direccion = new DirArriba();
+		bonecrusher.transformarse();
+		bonecrusher.moverEnDireccion(direccion, mapa);	
+		assertTrue(bonecrusher.getVida() == 200);
+		assertTrue(bonecrusher.getAtaque() == 30);
+		assertTrue(bonecrusher.getDistanciaDeAtaque() == 3);
+		assertTrue(bonecrusher.getVelocidad() == 8);		
+	}
 	
 	@Test(expected = HumanoideNoPuedeAtravezarException.class)
 	public void testOptimusHumanoideNoAtraviezaZonaPantano(){
