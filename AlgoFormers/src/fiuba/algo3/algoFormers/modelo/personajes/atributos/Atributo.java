e8bb0f055882a6d2f0566832b61f4eaeac3e939b
facundo <facundo@facundo-System-Product-Name>
2016-06-16 22:33:59 -0300

    Cree un manejador de efectos temporales para distribuir mejor el metodo calcular Valor actual

--- AlgoFormers/src/fiuba/algo3/algoFormers/modelo/personajes/atributos/Atributo.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/modelo/personajes/atributos/Atributo.java
@@ -7,18 +7,21 @@ import java.util.Set;
 
 import fiuba.algo3.algoFormers.modelo.efecto.EfectoEstatico;
 import fiuba.algo3.algoFormers.modelo.efecto.EfectoTemporal;
+import fiuba.algo3.algoFormers.modelo.personajes.manejadores.ManejadorDeEfectosTemporales;
 
 public abstract class Atributo {
 	
 	protected ArrayList<EfectoEstatico> efectosEstaticos;
-	protected HashMap<EfectoTemporal, Integer> efectosTemporales;
+	protected ManejadorDeEfectosTemporales manejadorEfectosTemporales;
+	//protected HashMap<EfectoTemporal, Integer> efectosTemporales;
 	protected int valorActual;
 	protected int valorOriginal;
 	private int valorCalculado;//Esto esta aca por el scope de lambda expression
 	
 	public Atributo(int valor){
 		efectosEstaticos = new ArrayList<EfectoEstatico>();
-		efectosTemporales = new HashMap<EfectoTemporal, Integer>();
+		//efectosTemporales = new HashMap<EfectoTemporal, Integer>();
+		manejadorEfectosTemporales = new ManejadorDeEfectosTemporales();
 		valorOriginal = valor;
 		valorActual = valor;
 	}
@@ -30,7 +33,7 @@ public abstract class Atributo {
 	}
 	
 	public void agregarEfectoTemporal(EfectoTemporal efecto){
-		this.efectosTemporales.put(efecto, efecto.turnos());
+		this.manejadorEfectosTemporales.agregarEfecto(efecto, efecto.turnos());
 		this.valorActual = this.calcularValorActual(this.valorActual);
 	}	
 	
@@ -49,24 +52,11 @@ public abstract class Atributo {
 			this.valorCalculado += efecto.obtenerValorFijo();
 			this.valorCalculado += (valorBase*efecto.obtenerValorMult())/100; 
 		}
-		Set<EfectoTemporal> efectos = this.efectosTemporales.keySet();
-		Iterator<EfectoTemporal> iter = efectos.iterator();
-		HashMap<EfectoTemporal, Integer> efectosActualizados = new HashMap<EfectoTemporal, Integer>();
-		while (iter.hasNext()) {
-		    EfectoTemporal efecto = iter.next();
-		    this.valorCalculado += efecto.obtenerValorFijo();
-			this.valorCalculado += (valorBase*efecto.obtenerValorMult())/100;
-			int turnos = this.efectosTemporales.get(efecto);
-			if (turnos - 1 == 0){
-				this.efectosTemporales.remove(efecto);
-			} else {efectosActualizados.put(efecto, turnos - 1);}		    
-		}
 		
-		this.efectosTemporales = efectosActualizados;
-		
+		this.valorCalculado = this.manejadorEfectosTemporales.calcularValorActual(valorBase,
+				this.valorCalculado);
 		//Para la nebulosa de andromeda, el valor calculado va a ser negativo y por eso se restaura a cero
 		if (valorCalculado < 0) valorCalculado = 0;
-		
 		return valorCalculado;
 	}
 	
