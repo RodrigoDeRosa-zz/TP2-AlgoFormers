8a67c8b1feb2d4ed2adba892e14593b882436b03
marc <marc@localhost>
2016-06-21 19:25:15 -0300

    tablero agregado a su propio contenedor

--- AlgoFormers/src/fiuba/algo3/algoFormers/vista/eventos/ControladorBienvenida.java
+++ AlgoFormers/src/fiuba/algo3/algoFormers/vista/eventos/ControladorBienvenida.java
@@ -13,6 +13,7 @@ import fiuba.algo3.algoFormers.vista.botones.acciones.BotonFinalizarTurno;
 import fiuba.algo3.algoFormers.vista.botones.acciones.BotonMover;
 import fiuba.algo3.algoFormers.vista.botones.acciones.BotonTransformar;
 import fiuba.algo3.algoFormers.vista.contenedores.ContenedorBotonesPrincipal;
+import fiuba.algo3.algoFormers.vista.contenedores.ContenedorTablero;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -58,9 +59,8 @@ public class ControladorBienvenida {
     	BorderPane contenedorJuego = (BorderPane) loader.load();
     	//Se busca en el centro donde hay guardado un GridPane de 40*40
     	ScrollPane contenedorCentro = (ScrollPane) contenedorJuego.getCenter();
-    	GridPane tablero = new GridPane();
-    	
-    	this.armarTablero(tablero, juego);
+    	ContenedorTablero tablero = new ContenedorTablero();
+    	tablero.armarTablero(juego);
     	contenedorCentro.setContent(tablero);
     	contenedorJuego.setCenter(contenedorCentro);
     	this.setBotonera(contenedorJuego);
@@ -88,52 +88,6 @@ public class ControladorBienvenida {
 		ContenedorBotonesPrincipal botones = new ContenedorBotonesPrincipal(mover, atacar, transformar, combinar, finalizarTurno);
 		
 		stage.setBottom(botones);		
-	}
-    
-	private void armarTablero(GridPane tablero, Juego juego) {
-		for(int i = 0; i < 40; i++){
-    		for(int j = 0; j < 40; j++){
-    			Posicion posicion = new Posicion(i,j);
-    			SuperficieTerrestre supTerrestre = juego.getSuperficieTerrestre(posicion);
-    			SuperficieAerea supAerea = juego.getSuperficieAerea(posicion);
-    			AlgoFormer algoformer = (AlgoFormer) juego.getAtacable(posicion);
-    			AnchorPane casillero = new AnchorPane();
-    			Capturable capturable = juego.getCapturable(posicion);
-    			
-    			//se dibujan y setean la superficies
-    			String nombreSupT = supTerrestre.getNombre();
-    			Image imagenTerrestre = new Image(("file:src/fiuba/algo3/algoFormers/vista/imagenes/" + nombreSupT + ".png"));
-    	        String nombreSupA = supAerea.getNombre();
-    	        if (nombreSupA != "Nube"){
-    	        	//se define la terrestre como fondo
-    	        	BackgroundImage imagenDeFondo = new BackgroundImage(imagenTerrestre, BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, BackgroundSize.DEFAULT);
-        	        casillero.setBackground(new Background(imagenDeFondo));
-    	        	//se superpone la aerea
-        	        ImageView imagenAerea = new ImageView(new Image(("file:src/fiuba/algo3/algoFormers/vista/imagenes/" + nombreSupA + ".png")));
-    	        	casillero.getChildren().add(imagenAerea);
-    	        }else{
-    	        	//solo de define la terrestre
-    	        	ImageView imagen = new ImageView(imagenTerrestre);
-    	        	casillero.getChildren().add(imagen);
-    	        }
-    	        
-    	        //se agrega al algoformer
-    	        if(algoformer != null){
-    	        	String nombreAlgoformer = algoformer.getNombre();
-    	        	ImageView imagenAlgoformer = new ImageView(new Image(("file:src/fiuba/algo3/algoFormers/vista/imagenes/" + nombreAlgoformer + ".png")));
-    	        	casillero.getChildren().add(imagenAlgoformer);
-    	        }
-    	        
-    	        //se agrega al bonus
-    	        if(capturable != null){
-    	        	String nombreCapturable = capturable.getNombre();
-    	        	ImageView imagenCapturable = new ImageView(new Image(("file:src/fiuba/algo3/algoFormers/vista/imagenes/" + nombreCapturable + ".png")));
-    	        	casillero.getChildren().add(imagenCapturable);
-    	        }
-    	        
-    			tablero.add(casillero, i, j);
-    		}
-		}	
 	}
 
 	public void initData(Juego juego) {
